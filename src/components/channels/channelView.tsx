81d065052a5d624fe4e7e17bcf21e407c58e4423
Vilan <lahman.digital@gmail.com>
2018-05-11 16:48:21 +0300

    Add services status labels

--- src/components/channels/channelView.tsx
+++ src/components/channels/channelView.tsx
@@ -1,4 +1,6 @@
 import * as React from 'react';
+import ChannelStatusStyle from './channelStatusStyle';
+import ContractStatus from './contractStatus';
 // import ChannelStatus from './channelStatus';
 // import EndpointView from '../endpoints/endpointView';
 
@@ -9,23 +11,29 @@ export default function(props:any){
         endpoint: <EndpointView channelId={props.channel.id} />
     </div>;
 */
+
+    // Need to use real Contract status
+    const contractRealStatus = 'in_challenge';
+
     return <div className='col-lg-9 col-md-8 col-sm-12 col-xs-12 m-b-20'>
         <div className='card m-b-20'>
             <h5 className='card-header'>General info</h5>
             <div className='col-md-12 col-sm-12 col-xs-12 p-0'>
                 <div className='card-body'>
-                    <table className='table table-striped'>
-                        <tbody>
-                            <tr><td width='30%'>Id:</td><td>{props.channel.id}</td></tr>
-                            <tr><td>Server:</td><td>[[ server ]]</td></tr>
-                            <tr><td>Offering:</td><td>{props.channel.offering}</td></tr>
-                            <tr><td>Contract Status:</td><td>{props.channel.channelStatus}</td></tr>
-                            <tr><td>Service Status:</td><td>{props.channel.serviceStatus}</td></tr>
-                            <tr><td>Usage:</td><td>[[ 120 Mb ]]</td></tr>
-                            <tr><td>Income:</td><td>[[ 14 PRIX ]]</td></tr>
-                            <tr><td>Deposit:</td><td>{props.channel.totalDeposit}</td></tr>
-                        </tbody>
-                    </table>
+                    <div className='table-responsive'>
+                        <table className='table table-striped'>
+                            <tbody>
+                                <tr><td width='30%'>Id:</td><td>{props.channel.id}</td></tr>
+                                <tr><td>Server:</td><td>[[ server ]]</td></tr>
+                                <tr><td>Offering:</td><td>{props.channel.offering}</td></tr>
+                                <tr><td>Contract Status:</td><td><ContractStatus contractStatus={contractRealStatus} /></td></tr>
+                                <tr><td>Service Status:</td><td><ChannelStatusStyle serviceStatus={props.channel.serviceStatus} /></td></tr>
+                                <tr><td>Usage:</td><td>[[ 120 Mb ]]</td></tr>
+                                <tr><td>Income:</td><td>[[ 14 PRIX ]]</td></tr>
+                                <tr><td>Deposit:</td><td>{props.channel.totalDeposit}</td></tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
@@ -34,13 +42,15 @@ export default function(props:any){
             <h5 className='card-header'>Access info</h5>
             <div className='col-md-12 col-sm-12 col-xs-12 p-0'>
                 <div className='card-body'>
-                    <table className='table table-striped'>
-                        <tbody>
-                            <tr><td width='30%'>Country:</td><td><img src='images/country/ua.png' width='30px'/></td></tr>
-                            <tr><td>Hostname:</td><td>[[ ip ]]</td></tr>
-                            <tr><td>port:</td><td>[[ 443 ]]</td></tr>
-                        </tbody>
-                    </table>
+                    <div className='table-responsive'>
+                        <table className='table table-striped'>
+                            <tbody>
+                                <tr><td width='30%'>Country:</td><td><img src='images/country/ua.png' width='30px'/></td></tr>
+                                <tr><td>Hostname:</td><td>[[ ip ]]</td></tr>
+                                <tr><td>port:</td><td>[[ 443 ]]</td></tr>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
