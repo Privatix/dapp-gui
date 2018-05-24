6f162010614e808610b786aa0d0a1446f06f5265
Timur Appaev <alloaolla@gmail.com>
2018-05-25 00:44:39 +0300

    search by settings, draft

--- src/components/settings.tsx
+++ src/components/settings.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import {fetch} from 'utils/fetch';
 import {asyncReactor} from 'async-reactor';
 import { withRouter } from 'react-router-dom';
+import SettingsTable from './settingsTable';
 
 function Loader() {
 
@@ -11,23 +12,8 @@ function Loader() {
 
 async function AsyncSettings(props:any){
 
-    const saveOptions = function(evt:any){
-        evt.preventDefault();
-        const inputs = document.getElementById('optionsForm').querySelectorAll('input');
-        const payload = [].slice.call(inputs, 0).map(option => ({
-            key: option.id
-           ,value: option.value
-           ,description: option.dataset.desc
-           ,name: option.dataset.name
-        }));
-        fetch('/settings', {method: 'put', body: payload}).then(res => {
-            // TODO notice?
-            props.history.push('/app');
-        });
-    };
-
-    return await fetch('/settings', {method: 'GET'}).then(options => {
-
+    const settings = await fetch('/settings', {method: 'GET'});
+/*
         const optionsDOM = (options as any).map(option => <tr>
                                         <td>{option.name}:</td>
                                         <td>
@@ -38,47 +24,23 @@ async function AsyncSettings(props:any){
                                         </td>
                                         <td>{option.description}</td>
                                     </tr>);
-        return (
-            <div className='container-fluid'>
-                <div className='row'>
-                    <div className='col-sm-12 m-b-15'>
-                        <h3 className='page-title'>Settings</h3>
+
+  const columns = [{
+    Header: 'Name',
+    accessor: 'name' // String-based value accessors!
+  }, {
+    Header: 'Val',
+    accessor: 'value',
+    Cell: props => <div className='form-control text-right'>
+                        <input id={props.key} type='text' defaultValue={props.value} data-desc={props.description} data-name={props.name}/>
+                        <label htmlFor={props.key} className='m-b-15'></label>
                     </div>
-                </div>
-                <div className='row'>
-                    <div className='col-12'>
-                        <div className='card-box'>
-                            <div className='form-group row'>
-                                <div className='col-md-12 m-t-10 m-b-10'>
-                                    <div className='input-group searchInputGroup'>
-                                        <div className='input-group-prepend'>
-                                            <span className='input-group-text'><i className='fa fa-search'></i></span>
-                                        </div>
-                                        <input className='form-control' type='search' name='search' placeholder='search' />
-                                    </div>
-                                </div>
-                            </div>
-                            <form id='optionsForm'>
-                                <table className='table table-bordered table-striped'>
-                                    <thead>
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>Value</th>
-                                        <th>Description</th>
-                                    </tr>
-                                    </thead>
-                                    <tbody>
-                                        {optionsDOM}
-                                    </tbody>
-                                </table>
-                                <button className='btn btn-default waves-effect waves-light' onClick={saveOptions}>Save</button>
-                            </form>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    });
+  }, {
+    Header: 'desc',
+    accessor: 'description' // Custom value accessors!
+  }];
+ */
+        return <SettingsTable options={settings} />;
 }
 
 export default withRouter(asyncReactor(AsyncSettings, Loader));
