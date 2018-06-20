8d9e6e25934c324ccbcb7e5b466933400826d0bd
Andrew Andreev <for.drew@gmail.com>
2018-06-20 15:19:06 +0300

    fix build issues

--- src/components/settingsTable.tsx
+++ src/components/settingsTable.tsx
@@ -1,25 +1,24 @@
 import * as React from 'react';
 import { withRouter } from 'react-router-dom';
-import ReactTable from 'react-table';
 
 // export default withRouter(function AsyncSettings(props:any){
-class SettingsCell extends React.Component<any, any> {
-    constructor(props: any) {
-        super(props);
-        this.state = {key: props.key, value: props.value, description: props.description, name: props.name};
-    }
-
-    static getDerivedStateFromProps(props: any, prevState: any){
-        return {key: props.key, value: props.value, description: props.description, name: props.name};
-    }
-
-    render(){
-        return <div className='form-control text-right'>
-                    <input id={this.state.key} type='text' defaultValue={this.state.value} data-desc={this.state.description} data-name={this.state.name}/>
-                    <label htmlFor={this.state.key} className='m-b-15'></label>
-                </div>;
-    }
-}
+// class SettingsCell extends React.Component<any, any> {
+//     constructor(props: any) {
+//         super(props);
+//         this.state = {key: props.key, value: props.value, description: props.description, name: props.name};
+//     }
+//
+//     static getDerivedStateFromProps(props: any, prevState: any){
+//         return {key: props.key, value: props.value, description: props.description, name: props.name};
+//     }
+//
+//     render(){
+//         return <div className='form-control text-right'>
+//                     <input id={this.state.key} type='text' defaultValue={this.state.value} data-desc={this.state.description} data-name={this.state.name}/>
+//                     <label htmlFor={this.state.key} className='m-b-15'></label>
+//                 </div>;
+//     }
+// }
 class SettingsTable extends React.Component<any, any> {
 
     constructor(props: any) {
@@ -73,7 +72,7 @@ class SettingsTable extends React.Component<any, any> {
 
     render(){
         console.log('RENEDER', this.state);
-        const { data } = this.state;
+        // const { data } = this.state;
         const optionsDOM = (this.state.data as any).map(option => <tr>
                                         <td>{option.name}:</td>
                                         <td>
@@ -85,60 +84,60 @@ class SettingsTable extends React.Component<any, any> {
                                         <td>{option.description}</td>
                                     </tr>);
 
-  const columns = [{
-    Header: 'Name',
-    accessor: 'name' // String-based value accessors!
-  }, {
-    Header: 'Val',
-    accessor: 'value',
-    Cell: <SettingsCell /> /* props => <div className='form-control text-right'>
-                        <input id={props.key} type='text' defaultValue={props.value} data-desc={props.description} data-name={props.name}/>
-                        <label htmlFor={props.key} className='m-b-15'></label>
-                    </div> */,
-    filterAll: true
-  }, {
-    Header: 'desc',
-    accessor: 'description' // Custom value accessors!
-  },
-  {
-              // NOTE - this is a "filter all" DUMMY column
-              // you can't HIDE it because then it wont FILTER
-              // but it has a size of ZERO with no RESIZE and the
-              // FILTER component is NULL (it adds a little to the front)
-              // You culd possibly move it to the end
-              Header: 'All',
-              id: 'all',
-              width: 0,
-              resizable: false,
-              sortable: true,
-              Filter: () => { /* */ },
-              getProps: () => {
-                return {
-                  // style: { padding: "0px"}
-                };
-              },
-              filterMethod: (filter, rows) => {
-                  console.log('FILTER!!!', filter, rows);
-
-                  const res = rows.filter(row => {
-                      if(row.description.indexOf(filter.value) !== -1){
-                          console.log(row.description, filter.value);
-                      }
-                      if(row.name.indexOf(filter.value) !== -1){
-                          console.log(row.name, filter.value);
-                      }
-                      if(row.value.indexOf(filter.value) !== -1){
-                          console.log(row.value, filter.value);
-                      }
-                      return row.description.indexOf(filter.value) !== -1 ||  row.name.indexOf(filter.value) !== -1 || row.value.indexOf(filter.value) !== -1;
-                  }
-                  );
-                  console.log('RESULT!!!', res);
-                  return res;
-              },
-              filterAll: true
-            }
-  ];
+  // const columns = [{
+  //   Header: 'Name',
+  //   accessor: 'name' // String-based value accessors!
+  // }, {
+  //   Header: 'Val',
+  //   accessor: 'value',
+  //   Cell: <SettingsCell /> /* props => <div className='form-control text-right'>
+  //                       <input id={props.key} type='text' defaultValue={props.value} data-desc={props.description} data-name={props.name}/>
+  //                       <label htmlFor={props.key} className='m-b-15'></label>
+  //                   </div> */,
+  //   filterAll: true
+  // }, {
+  //   Header: 'desc',
+  //   accessor: 'description' // Custom value accessors!
+  // },
+  // {
+  //             // NOTE - this is a "filter all" DUMMY column
+  //             // you can't HIDE it because then it wont FILTER
+  //             // but it has a size of ZERO with no RESIZE and the
+  //             // FILTER component is NULL (it adds a little to the front)
+  //             // You culd possibly move it to the end
+  //             Header: 'All',
+  //             id: 'all',
+  //             width: 0,
+  //             resizable: false,
+  //             sortable: true,
+  //             Filter: () => { /* */ },
+  //             getProps: () => {
+  //               return {
+  //                 // style: { padding: "0px"}
+  //               };
+  //             },
+  //             filterMethod: (filter, rows) => {
+  //                 console.log('FILTER!!!', filter, rows);
+  //
+  //                 const res = rows.filter(row => {
+  //                     if(row.description.indexOf(filter.value) !== -1){
+  //                         console.log(row.description, filter.value);
+  //                     }
+  //                     if(row.name.indexOf(filter.value) !== -1){
+  //                         console.log(row.name, filter.value);
+  //                     }
+  //                     if(row.value.indexOf(filter.value) !== -1){
+  //                         console.log(row.value, filter.value);
+  //                     }
+  //                     return row.description.indexOf(filter.value) !== -1 ||  row.name.indexOf(filter.value) !== -1 || row.value.indexOf(filter.value) !== -1;
+  //                 }
+  //                 );
+  //                 console.log('RESULT!!!', res);
+  //                 return res;
+  //             },
+  //             filterAll: true
+  //           }
+  // ];
         return (
             <div className='container-fluid'>
                 <div className='row'>
@@ -149,7 +148,6 @@ class SettingsTable extends React.Component<any, any> {
                 <div className='row'>
                     <div className='col-12'>
                         <div className='card-box'>
-                            {/*TODO(drew2a) hide search on alpha*/}
                             {/*<div className='form-group row'>*/}
                                 {/*<div className='col-md-12 m-t-10 m-b-10'>*/}
                                     {/*<div className='input-group searchInputGroup'>*/}
