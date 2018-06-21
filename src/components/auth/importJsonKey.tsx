9a73b4181c2b8c75b6b5c1582d9abe8a13ca1683
Borys Rubel <borys@privatix.io>
2018-06-21 13:10:57 +0300

    message about the wrong password

--- src/components/auth/importJsonKey.tsx
+++ src/components/auth/importJsonKey.tsx
@@ -51,7 +51,13 @@ class ImportJsonKey extends React.Component<any, any>{
         const res = await fetch('/readFile', {method: 'post', body: {fileName}});
         const keyObject = JSON.parse((res as any).file);
         console.log(pwd, fileName, res, keyObject);
-        const pk = keythereum.recover(pwd, keyObject);
+        let pk;
+        try {
+            pk = keythereum.recover(pwd, keyObject);
+        } catch (err) {
+            msg = 'Please enter a valid password.';
+            notice({level: 'error', header: 'attention!', msg});
+        }
         const key = pk.toString('base64').split('+').join('-').split('/').join('_');
         console.log(pk, key);
 
@@ -61,7 +67,6 @@ class ImportJsonKey extends React.Component<any, any>{
                      ,name
                      ,type: 'generate_new'
         };
-        console.log(body);
         await fetch('/accounts/', {method: 'post', body});
 
         const settings = await fetch('/localSettings', {}) as any;
