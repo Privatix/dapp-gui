f0a69fecbf4faa041a291a6f0d83d493ece465ec
Timur Appaev <alloaolla@gmail.com>
2018-06-28 12:31:29 +0300

    creating components for modal window

--- src/components/modalWindow.tsx
+++ src/components/modalWindow.tsx
@@ -34,35 +34,40 @@ export default class ModalWindow extends React.Component<any, any> {
     }
 
     render(){
+
         if (this.state.visible === true) {
             document.body.classList.add('modal-open');
         }
-        
+
+        const content = this.state.visible
+            ? <div className='modalWrap'>
+                  <Modal
+                      visible={this.state.visible}
+                      width='90%'
+                      effect='fadeInUp'
+                      onClickAway={this.closeModal.bind(this)}
+                  >
+                      <div className='modal-content'>
+                          <div className='modal-header'>
+                              <h4 className='modal-title'>{this.state.props.modalTitle}</h4>
+                              <button type='button' className='close' onClick={this.closeModal.bind(this)}>×</button>
+                          </div>
+                          <div className='modal-body'>
+                              {React.cloneElement(this.state.props.component, {closeModal: this.closeModal.bind(this)
+                                                                              ,render: this.changeContent.bind(this)
+                                                                              ,visible: this.state.visible
+                                                                              }
+                              )}
+                          </div>
+                      </div>
+                  </Modal>
+              </div>
+            : <div></div>;
+
         return (
             <div>
                 <a href='#' onClick={this.showModal.bind(this)} className={this.props.customClass}>{this.props.text}</a>
-                <div className='modalWrap'>
-                    <Modal
-                        visible={this.state.visible}
-                        width='90%'
-                        effect='fadeInUp'
-                        onClickAway={this.closeModal.bind(this)}
-                    >
-                        <div className='modal-content'>
-                            <div className='modal-header'>
-                                <h4 className='modal-title'>{this.state.props.modalTitle}</h4>
-                                <button type='button' className='close' onClick={this.closeModal.bind(this)}>×</button>
-                            </div>
-                            <div className='modal-body'>
-                                {React.cloneElement(this.state.props.component, {closeModal: this.closeModal.bind(this)
-                                                                                ,render: this.changeContent.bind(this)
-                                                                                ,visible: this.state.visible
-                                                                                }
-                                )}
-                            </div>
-                        </div>
-                    </Modal>
-                </div>
+                {content}
             </div>
         );
     }
