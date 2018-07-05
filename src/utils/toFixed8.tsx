66207bd7d6c5580e0ac1b6b9754d01e2b2283785
Vilan <viktor@privatix.io>
2018-07-05 16:37:13 +0300

    Fix sessions statistics

--- src/utils/toFixed8.tsx
+++ src/utils/toFixed8.tsx
@@ -1,5 +1,10 @@
 export default function (props: any) {
     const number = props.number;
+
+    if (typeof number === 'undefined' || number === null) {
+        return 0;
+    }
+
     const isHaveDecimals = Math.floor(number) !== number;
 
     return isHaveDecimals ? number.toFixed(8).replace(/0+$/, '') : number;
