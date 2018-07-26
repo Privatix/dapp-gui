58c5d5107790c1c27f4a985c6035b08b5915525a
Timur Appaev <alloaolla@gmail.com>
2018-07-26 12:18:12 +0300

    text replaced

--- src/components/channels/channelTools.tsx
+++ src/components/channels/channelTools.tsx
@@ -4,18 +4,17 @@ import * as React from 'react';
 import ConfirmPopupSwal from '../confirmPopupSwal';
 
 export default function(props:any){
-
+    const info = 'This operation will terminate service and close contract. Earnings will be transferred to your account.';
     return <div className='col-lg-3 col-md-4'>
         <div className='card m-b-20 card-body text-xs-center warningAreaCard'>
             <form>
                 <h5 className='card-title'>Warning Area</h5>
-                <p className='card-text'>This operation will terminate the service and call the "Uncooperative close" procedure</p>
+                <p className='card-text'>{info}</p>
                 <ConfirmPopupSwal
                     endpoint={`/channels/${props.channel.id}/status`}
                     options={{method: 'put', body: {action: 'terminate'}}}
                     title={'Terminate contract'}
-                    text={<span>This operation will terminate the service and call the "Uncooperative close" procedure.<br />
-                            You can't undo this.</span>}
+                    text={<span>{info}</span>}
                     class={'btn btn-danger btn-custom btn-block'}
                     swalType='danger'
                     swalConfirmBtnText='Yes, terminate it!'
