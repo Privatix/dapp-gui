6ca3e9c1421009abbf17804aea8874c56b1e3ad6
Timur Appaev <alloaolla@gmail.com>
2018-07-31 15:32:34 +0300

    terminate contract button depends on service status

--- src/components/channels/channelTools.tsx
+++ src/components/channels/channelTools.tsx
@@ -13,21 +13,23 @@ export default function(props:any){
         ? 'Terminate service'
         : 'Terminate contract';
 
-    return <div className='col-lg-3 col-md-4'>
-        <div className='card m-b-20 card-body text-xs-center warningAreaCard'>
-            <form>
-                <h5 className='card-title'>Warning Area</h5>
-                <p className='card-text'>{info}</p>
-                <ConfirmPopupSwal
-                    endpoint={`/channels/${props.channel.id}/status`}
-                    options={{method: 'put', body: {action: 'terminate'}}}
-                    title={buttonTitle}
-                    text={<span>{info}</span>}
-                    class={'btn btn-danger btn-custom btn-block'}
-                    swalType='danger'
-                    swalConfirmBtnText='Yes, terminate it!'
-                    swalTitle='Are you sure?' />
-            </form>
+    return ['active', 'pending'].includes(props.channel.channelStatus)
+        ? <div className='col-lg-3 col-md-4'>
+            <div className='card m-b-20 card-body text-xs-center warningAreaCard'>
+                <form>
+                    <h5 className='card-title'>Warning Area</h5>
+                    <p className='card-text'>{info}</p>
+                    <ConfirmPopupSwal
+                        endpoint={`/channels/${props.channel.id}/status`}
+                        options={{method: 'put', body: {action: 'terminate'}}}
+                        title={buttonTitle}
+                        text={<span>{info}</span>}
+                        class={'btn btn-danger btn-custom btn-block'}
+                        swalType='danger'
+                        swalConfirmBtnText='Yes, terminate it!'
+                        swalTitle='Are you sure?' />
+                </form>
+            </div>
         </div>
-    </div>;
+        :<div></div>;
 }
