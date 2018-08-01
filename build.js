2fa3f6b68387f8ca23eebe65fe8c0cd18893f179
Vitaliy Gnidenko <i@lart.com.ua>
2018-08-01 11:06:29 +0300

    add version to settings.json on build project

--- build.js
+++ build.js
@@ -6,4 +6,23 @@ const start = config.scripts.start;
 config.scripts = {start};
 config.private = false;
 
+const p = [];
+const settings = require('./build/settings');
+p.push(new Promise((resolve, reject) => {
+    require('child_process').exec('git rev-parse HEAD', function(err, stdout) {
+        settings.commit  = stdout.trim();
+        resolve();
+    })
+}));
+p.push(new Promise((resolve, reject) => {
+    require('child_process').exec('git tag', function(err, stdout) {
+        settings.release = stdout.trim();
+        resolve();
+    })
+}));
+
+Promise.all(p).then(()=> {
+    fs.writeFileSync('./build/settings.json', JSON.stringify(settings, null, 4));
+});
+
 fs.writeFileSync('./build/package.json', JSON.stringify(config, null, 4));
