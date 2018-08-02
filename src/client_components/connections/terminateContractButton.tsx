fcc039df39022d0a129221587a883d16ae27bc0a
Timur Appaev <alloaolla@gmail.com>
2018-08-02 12:26:10 +0300

    client service view: button's text changed

--- src/client_components/connections/terminateContractButton.tsx
+++ src/client_components/connections/terminateContractButton.tsx
@@ -7,12 +7,17 @@ export default class TerminateContractButton extends React.Component<any, any>{
         return <div className='card m-b-20 card-body text-xs-center warningAreaCard'>
             <form>
                 <h5 className='card-title'>Warning Area</h5>
-                <p className='card-text'>This operation will terminate the service and call the "Uncooperative close" procedure</p>
+                <p className='card-text'>Permanently stop using service and request full deposit return.</p>
+                <p className='card-text'>Use only, if Agent doesn’t closes contract for very long time.<br />
+                    You pay multiple transaction fees.</p>
                 <ConfirmPopupSwal
                     endpoint={`/client/channels/${this.props.channelId}/status`}
                     options={{method: 'put', body: {action: 'terminate'}}}
                     title={'Terminate contract'}
-                    text={<span>This operation will terminate the service and call the "Uncooperative close" procedure</span>}
+                    text={<span>Permanently stop using service and request full deposit return.<br />
+                        Use only, if Agent doesn’t closes contract for very long time.<br />
+                        You pay multiple transaction fees.
+                    </span>}
                     class={'btn btn-danger btn-custom btn-block'}
                     swalType='danger'
                     swalConfirmBtnText='Yes, terminate contract!'
