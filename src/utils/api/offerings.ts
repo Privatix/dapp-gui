1c2a8104acdad5bf61a5b01692ba5febf057ce15
Vitaliy Gnidenko <i@lart.com.ua>
2018-08-06 14:22:57 +0300

    PUT /client/offerings/{id}/status

--- src/utils/api/offerings.ts
+++ src/utils/api/offerings.ts
@@ -1,5 +1,6 @@
 import {fetch} from '../fetch';
 import {OfferStatus, Offering} from '../../typings/offerings';
+import {SaveAnswer} from '../../typings/SaveAnswer';
 
 export const getOfferings  = async function(id?:string, product?:string, offerStatus?:OfferStatus): Promise<Offering[]>{
     let query = [];
@@ -7,4 +8,12 @@ export const getOfferings  = async function(id?:string, product?:string, offerSt
     if (product){ query.push(`product=${product}`);}
     if (offerStatus){ query.push(`offerStatus=${offerStatus}`);}
     return fetch('/offerings'+(query.length>0?'?'+query.join('&'):'')) as Promise<Offering[]>;
+};
+
+
+export const changeClientOfferingsStatus = async function(id: string, action: string, account: string, gasPrice: number): Promise<SaveAnswer>{
+	return fetch(`/client/offerings/${id}/status`, {
+		method: 'put',
+		body: {action: action, account: account, gasPrice: gasPrice}
+	}) as Promise<SaveAnswer>;
 };
