df9588ade0688ffda96ab0eba8054fb4e004f100
Vilan <viktor@privatix.io>
2018-09-20 14:06:21 +0300

    Fix channelTools translation functionality

--- src/components/channels/channelTools.tsx
+++ src/components/channels/channelTools.tsx
@@ -2,36 +2,38 @@ import * as React from 'react';
 
 // import ChannelToolClose from './channelToolClose';
 import ConfirmPopupSwal from '../confirmPopupSwal';
-import i18n from '../../i18next/init';
+import { translate } from 'react-i18next';
 
-export default function(props:any){
+export default translate('channels/channelView')(function(props: any) {
+        const { t } = props;
 
-    const warning = i18n.t('channels/channelView:YouCanNotUndoThis');
-    const info = props.channel.receiptBalance === 0
-        ? i18n.t('channels/channelView:TerminateServiceText')
-        : i18n.t('channels/channelView:TerminateServiceAndCloseContract');
+        const warning = t('YouCanNotUndoThis');
+        const info = props.channel.receiptBalance === 0
+            ? t('TerminateServiceText')
+            : t('TerminateServiceAndCloseContract');
 
-    const buttonTitle = props.channel.receiptBalance === 0
-        ? i18n.t('channels/channelView:TerminateService')
-        : i18n.t('channels/channelView:TerminateContract');
+        const buttonTitle = props.channel.receiptBalance === 0
+            ? t('TerminateService')
+            : t('TerminateContract');
 
-    return ['active', 'pending'].includes(props.channel.channelStatus) && props.channel.serviceStatus !== 'terminated'
-        ? <div className='col-lg-3 col-md-4'>
-            <div className='card m-b-20 card-body text-xs-center warningAreaCard'>
-                <form>
-                    <h5 className='card-title'>{i18n.t('channels/channelView:WarningArea')}</h5>
-                    <p className='card-text'>{info}</p>
-                    <ConfirmPopupSwal
-                        endpoint={`/channels/${props.channel.id}/status`}
-                        options={{method: 'put', body: {action: 'terminate'}}}
-                        title={buttonTitle}
-                        text={<span>{`${info} ${warning}`}</span>}
-                        class={'btn btn-danger btn-custom btn-block'}
-                        swalType='danger'
-                        swalConfirmBtnText={i18n.t('channels/channelView:YesTerminateIt')}
-                        swalTitle={i18n.t('channels/channelView:AreYouSure')} />
-                </form>
+        return ['active', 'pending'].includes(props.channel.channelStatus) && props.channel.serviceStatus !== 'terminated'
+            ? <div className='col-lg-3 col-md-4'>
+                <div className='card m-b-20 card-body text-xs-center warningAreaCard'>
+                    <form>
+                        <h5 className='card-title'>{t('WarningArea')}</h5>
+                        <p className='card-text'>{info}</p>
+                        <ConfirmPopupSwal
+                            endpoint={`/channels/${props.channel.id}/status`}
+                            options={{method: 'put', body: {action: 'terminate'}}}
+                            title={buttonTitle}
+                            text={<span>{`${info} ${warning}`}</span>}
+                            class={'btn btn-danger btn-custom btn-block'}
+                            swalType='danger'
+                            swalConfirmBtnText={t('YesTerminateIt')}
+                            swalTitle={t('AreYouSure')}/>
+                    </form>
+                </div>
             </div>
-        </div>
-        :<div></div>;
-}
+            : <div></div>;
+    }
+);
