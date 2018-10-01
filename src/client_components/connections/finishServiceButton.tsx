61a725ef1c73b06da8bed6f9c990187926afa440
Timur Appaev <alloaolla@gmail.com>
2018-10-01 15:33:38 +0300

    align buttons on dashboard

--- src/client_components/connections/finishServiceButton.tsx
+++ src/client_components/connections/finishServiceButton.tsx
@@ -9,31 +9,32 @@ export default class FinishServiceButton extends React.Component<any, any>{
     render(){
         const { t } = this.props;
 
-        return <div className='card m-b-20 card-body text-xs-center'>
-                <form>
-                    <p className='card-text'>{t('PermanentlyStopUsingService')}</p>
-                    <p className='card-text'>
+        return <div className='card m-b-20 card-body text-xs-center buttonBlock'>
+            <div>
+                <p className='card-text'>{t('PermanentlyStopUsingService')}</p>
+                <p className='card-text'>
+                    {this.props.channel.usage.cost === 0
+                        ? t('CanRequestFullDepositReturn')
+                        : t('RemainingDepositWillBeReturned')
+                    }
+                </p>
+            </div>
+            <ConfirmPopupSwal
+                endpoint={`/client/channels/${this.props.channel.id}/status`}
+                options={{method: 'put', body: {action: 'terminate'}}}
+                title={t('Finish')}
+                text={
+                    <span>{t('PermanentlyStopUsingService')}<br />
                         {this.props.channel.usage.cost === 0
                             ? t('CanRequestFullDepositReturn')
                             : t('RemainingDepositWillBeReturned')
                         }
-                    </p>
-                    <ConfirmPopupSwal
-                        endpoint={`/client/channels/${this.props.channel.id}/status`}
-                        options={{method: 'put', body: {action: 'terminate'}}}
-                        title={t('Finish')}
-                        text={
-                            <span>{t('PermanentlyStopUsingService')}<br />
-                                {this.props.channel.usage.cost === 0
-                                    ? t('CanRequestFullDepositReturn')
-                                    : t('RemainingDepositWillBeReturned')
-                                }
-                            </span>}
-                        class={'btn btn-primary btn-custom btn-block'}
-                        swalType='warning'
-                        swalConfirmBtnText={t('YesFinishIt')}
-                        swalTitle={t('AreYouSure')} />
-                </form>
-            </div>;
+                    </span>}
+                class={'btn btn-primary btn-custom btn-block'}
+                swalType='warning'
+                swalConfirmBtnText={t('YesFinishIt')}
+                swalTitle={t('AreYouSure')}
+            />
+        </div>;
     }
 }
--- src/client_components/connections/increaseDepositButton.tsx
+++ src/client_components/connections/increaseDepositButton.tsx
@@ -12,13 +12,15 @@ export default class IncreaseDepositButton extends React.Component<any, any>{
 
         const { t } = this.props;
 
-        return  <div className='card m-b-20 card-body text-xs-center'>
+        return  <div className='card m-b-20 card-body text-xs-center buttonBlock'>
             <p className='card-text'>{t('ThisOperationWillIncrease')}</p>
-            <ModalWindow customClass='btn btn-primary btn-custom btn-block'
-                         modalTitle={t('IncreaseDeposit')}
-                         text={t('IncreaseDeposit')}
-                         component={<IncreaseDepositView channel={this.props.channel} />}
-            />
+            <div>
+                <ModalWindow customClass='btn btn-primary btn-custom btn-block'
+                             modalTitle={t('IncreaseDeposit')}
+                             text={t('IncreaseDeposit')}
+                             component={<IncreaseDepositView channel={this.props.channel} />}
+                />
+            </div>
         </div>;
     }
 }
