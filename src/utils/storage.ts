9b606897b064086e146adf2b6c62ec548890b4c7
Timur Appaev <alloaolla@gmail.com>
2018-12-12 16:25:51 +0300

    refactror topPanel

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,7 +1,24 @@
 import { createStore, applyMiddleware } from 'redux';
 import thunkMiddleware from 'redux-thunk';
-import reducers from 'redux/reducers';
 
-export const store = createStore(reducers, applyMiddleware(
+import reducers from 'redux/reducers';
+import { asyncProviders } from 'redux/actions';
+
+const storage = createStore(reducers, applyMiddleware(
     thunkMiddleware, // lets us dispatch() functions
   ));
+
+const refresh = function(){
+
+    const { ws } = storage.getState();
+
+    if(ws){
+        storage.dispatch(asyncProviders.updateAccounts());
+    }
+
+    setTimeout(refresh, 3000);
+};
+
+refresh();
+
+export const store = storage;
