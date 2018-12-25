c38af3f4503c5e0ef4b0b18f7f7bef7b083cd944
drew2a <for.drew@gmail.com>
2018-12-25 14:10:06 +0300

    close modal by clicking on grey area

--- src/common/modalWindow.tsx
+++ src/common/modalWindow.tsx
@@ -4,6 +4,8 @@ import CopyToClipboard from './copyToClipboard';
 
 export default class ModalWindow extends React.Component<any, any> {
 
+    private content = React.createRef<HTMLDivElement>();
+
     constructor(props: any) {
 
         super(props);
@@ -16,6 +18,24 @@ export default class ModalWindow extends React.Component<any, any> {
         };
     }
 
+    skip = (event: any) => {
+        console.log(event);
+        if(['A', 'BUTTON'].includes(event.srcElement.tagName)){
+            return;
+        }
+        event.preventDefault();
+        event.stopPropagation();
+    }
+
+    closeModalTop = (event: any) => {
+
+        if(['A', 'BUTTON'].includes(event.srcElement.tagName)){
+            return;
+        }else{
+            this.closeModal(event);
+        }
+    }
+
     closeModal = (event: any) => {
 
         if(event){
@@ -23,6 +43,7 @@ export default class ModalWindow extends React.Component<any, any> {
         }
 
         document.body.classList.remove('modal-open');
+        document.removeEventListener('click', this.closeModalTop);
 
         const { component, modalTitle } = this.props;
         this.setState({
@@ -51,6 +72,14 @@ export default class ModalWindow extends React.Component<any, any> {
     showModal = (event: any) => {
         event.preventDefault();
         this.setState({visible: true});
+        document.addEventListener('click', this.closeModalTop);
+
+    }
+
+    componentDidUpdate(){
+        if(this.state.visible){
+            this.content.current.addEventListener('click', this.skip, false);
+        }
     }
 
     render() {
@@ -63,27 +92,27 @@ export default class ModalWindow extends React.Component<any, any> {
 
         const content = visible
             ? <div className='modalWrap'>
-                  <Modal
-                      visible={visible}
-                      width='90%'
-                      effect='fadeInUp'
-                      onClickAway={this.closeModal}
-                  >
-                      <div className='modal-content'>
-                          <div className='modal-header'>
-                              <h4 className='modal-title'>{this.state.modalTitle}</h4>
-                              <button type='button' className='close' onClick={this.closeModal}>×</button>
-                          </div>
-                          <div className='modal-body'>
-                              {React.cloneElement(this.state.component, {closeModal: this.closeModal
-                                                                        ,render: this.changeContent
-                                                                        ,visible
-                                                                        }
-                              )}
-                          </div>
-                      </div>
-                  </Modal>
-              </div>
+                <Modal
+                    visible={visible}
+                    width='90%'
+                    effect='fadeInUp'
+                    onClickAway={this.closeModal}
+                >
+                    <div ref={this.content} className='modal-content'>
+                        <div className='modal-header'>
+                            <h4 className='modal-title'>{this.state.modalTitle}</h4>
+                            <button type='button' className='close' onClick={this.closeModal}>×</button>
+                        </div>
+                        <div className='modal-body'>
+                            {React.cloneElement(this.state.component, {closeModal: this.closeModal
+                                    ,render: this.changeContent
+                                    ,visible
+                                }
+                            )}
+                        </div>
+                    </div>
+                </Modal>
+            </div>
             : <div></div>;
 
         const copyToClipboard = !props.copyToClipboard ? '' :
